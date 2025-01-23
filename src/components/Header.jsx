17b9bf0a3ee3cb652e8dc13196724480e2e7f5e2
mmkarkko <miia.arkko@gmail.com>
2025-01-23 10:29:38 +0200

    Add favicon. Change logo from png -> svg.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css";
 import Menu from "./Menu";
 import { FiMenu, FiX } from "react-icons/fi";
 import { useState } from "react";
-import logo from "../assets/arkkomatic_logo.png";
+import logo from "../assets/arkkoMatic-logo-shadow_text-as-path-optimized.svg";
 import { Link } from "react-router-dom";
 
 export default function Header() {

4e5c95c3b7d441ff82d6e4e545b7224f056f8748
mmkarkko <miia.arkko@gmail.com>
2025-01-26 18:10:06 +0200

    Try to revert changes

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,9 @@
+import "./ContactPage.css";
+
+export default function ContactPage() {
+  return (
+    <div className="contactPage">
+      <h2 id="contact-header">Yhteystiedot</h2>
+    </div>
+  );
+}
